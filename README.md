25f75ef9fc9c868297fdc94ff28ed9d0e9f2b869
Sylvester Willis <sylvester@ubuntu.(none)>
2014-05-08 14:35:28 -0700

    Initial creation of repo

---
+++ README.md
@@ -0,0 +1,4 @@
+Recipe Reminder
+===============
+
+Allows users to create recipes to keep track of recipes that they have made.
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Recipereminder::Application.config.session_store :cookie_store, key: '_recipereminder_session'
