5eab7e13685ceb9e567c99541fe176f7c2a844a4
Sylvester Willis <sylvester@ubuntu.(none)>
2014-05-15 18:45:49 -0700

    Refactored code within user registration tests into helper function.

--- spec/requests/create_users_spec.rb
+++ spec/requests/create_users_spec.rb
@@ -3,16 +3,9 @@ require 'spec_helper'
 describe "CreateUsers" do
 
   it "Emails user when account is created." do
-    testEmail = 'testguy@someplace.net'
-    testPassword = 'password'
-    visit new_user_registration_path
-    fill_in :user_first_name, :with => "First"
-    fill_in :user_last_name, :with => "Last"
-    fill_in :user_email, :with => testEmail
-    fill_in :user_password, :with => testPassword
-    fill_in :user_password_confirmation, :with => testPassword
-    click_button "Sign up"
-   
+    user = FactoryGirl.build(:user)
+    
+    register_user(user) 
     #Default route after registration is root 
     current_path.should eq('/')
   end
@@ -20,14 +13,8 @@ describe "CreateUsers" do
   it "Does not email user when duplicate account is created." do
     user = FactoryGirl.create(:user)
 
-    # Try to create an accoutn with test user info.
-    visit new_user_registration_path
-    fill_in :user_first_name, :with => "First"
-    fill_in :user_last_name, :with => "Last"
-    fill_in :user_email, :with => user.email
-    fill_in :user_password, :with => user.password
-    fill_in :user_password_confirmation, :with => user.password
-    click_button "Sign up"
+    # Try to create an account with test user info.
+    register_user(user) 
     page.should have_content "Email has already been taken"
   end
 
